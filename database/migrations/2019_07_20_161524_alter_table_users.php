d1370a495b6766195fb07e32310485a418ee181a
Oleg Savitskiy <savitoleg@ukr.net>
2019-07-20 19:38:17 +0300

    v1.3 alter table users migrate alter_table_user

---
+++ database/migrations/2019_07_20_161524_alter_table_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //додавання поля is_admin після стовпця password
+            $table->boolean('is_admin')->default(0)->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //видалення поля is_admin
+            $table->dropColumn('is_admin');
+        });
+    }
+}
